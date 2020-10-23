bce3f79d05d999f3a289b9ba85933b50904ff032
Bernhard Windisch <bernhard.windisch@doob.at>
2020-10-23 07:32:12 +0200

    implement Attachments, move WellKnown Pbjects to SharedModels

---
+++ source/ScsmClient/Operations/AttachmentOperations.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using Microsoft.EnterpriseManagement.Common;
+using Microsoft.EnterpriseManagement.Configuration;
+using Microsoft.EnterpriseManagement.ConnectorFramework;
+using Reflectensions.ExtensionMethods;
+using ScsmClient.Caches;
+
+namespace ScsmClient.Operations
+{
+    public class AttachmentOperations : BaseOperation
+    {
+        
+        private List<string> _allowedClassesForAttachments = new List<string>()
+        {
+            "System.WorkItem"
+        };
+
+        public AttachmentOperations(SCSMClient client) : base(client)
+        {
+        }
+
+        public Guid AddAttachment(Guid sourceId, string displayName, Stream content, string description)
+        {
+            var sourceObject = _client.Object().GetEnterpriseManagementObjectById(sourceId);
+            return AddAttachment(sourceObject, displayName, content, description);
+        }
+
+        public Guid AddAttachment(EnterpriseManagementObject sourceObject, string displayName, Stream content, string description)
+        {
+            var fileAttachmentClass = _client.Types().GetClassByName("System.FileAttachment");
+            var obj = new CreatableEnterpriseManagementObject(_client.ManagementGroup, fileAttachmentClass);
+
+            obj[fileAttachmentClass, "Id"].Value = Guid.NewGuid().ToString();
+            obj[fileAttachmentClass, "DisplayName"].Value = displayName;
+            obj[fileAttachmentClass, "Content"].Value = content;
+            obj[fileAttachmentClass, "Size"].Value = content.Length;
+            obj[fileAttachmentClass, "Description"].Value = description;
+            obj[fileAttachmentClass, "AddedDate"].Value = DateTime.UtcNow;
+
+            if (displayName.Contains('.'))
+            {
+                obj[fileAttachmentClass, "Extension"].Value = displayName.Split('.').Last();
+            }
+
+            _client.Relations().CreateRelation(sourceObject, obj);
+            return obj.Id;
+        }
+
+
+        public IEnumerable<EnterpriseManagementObject> GetAttachments(Guid sourceId)
+        {
+            return _client.Relations().GetRelatedObjectsByClassName(sourceId, "System.FileAttachment");
+        }
+    }
+}
