95c7c356fbf4dfabbf275d88a7ad701392a73cc1
windischb <bernhard.windisch@doob.at>
2021-06-14 15:18:34 +0200

    cleanup

--- source/ScsmClient/ExtensionMethods/ManagementPackClassPropertyExtensions.cs
+++ source/ScsmClient/ExtensionMethods/ManagementPackClassPropertyExtensions.cs
@@ -3,8 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using doob.Reflectensions.Common;
 using Microsoft.EnterpriseManagement.Configuration;
-using Reflectensions.ExtensionMethods;
 using ScsmClient.Model;
 
 namespace ScsmClient.ExtensionMethods
